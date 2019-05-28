87fc6970df47eaea966ea5b1d113b12a610152ae
khainan <muhammadrachim.k@gmail.com>
2019-05-29 01:34:10 +0700

    add dummy dotted header (not responsive

--- src/pages/ChatatPages.jsx
+++ src/pages/ChatatPages.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import dotted from '../assets/img/dotted_sc.png';
 
 class MainPage extends Component {
   constructor(props) {
@@ -8,12 +9,14 @@ class MainPage extends Component {
 
     };
   }
-
  
   render() {
+
     return (
         <div className="catat-main-page">
-            tes
+            <div className="header-page">
+                <img src={dotted}/>
+            </div>
         </div>
     );
   }
