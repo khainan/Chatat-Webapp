0bcab57b3742fa10576ee93ca08efeb2d2faaa6d
khainan <muhammadrachim.k@gmail.com>
2019-05-28 21:32:53 +0700

    add scss

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import '../scss/main-page.scss'
+import axios from "axios";
+
+class ListCycle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+ 
+  render() {
+    return (
+      <div className="catat-page">
+          <div className="catat-container-page">
+            <div className="left-bar"></div>
+            <div className="sidebar-catat">tes</div>
+            <div className="catat-main-page">tes</div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default ListCycle;
