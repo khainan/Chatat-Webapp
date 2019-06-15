a16486f52cb2e4cb687f5dba0f30b97e42a9d712
Hari <neogalaxy45@gmail.com>
2019-06-15 16:47:35 +0700

    change route

--- src/UnAuthenticatedApp.js
+++ src/UnAuthenticatedApp.js
@@ -6,14 +6,20 @@ import LoginPage from './pages/LoginPage';
 import ResetPassword from './pages/ResetPassword';
 import RegisterPage from './pages/RegisterPage';
 
+const PublicRoute = ({ component: Component, ...rest }) => (
+	<Route {...rest} render={(props) => (
+		<Component {...props} />
+	)} />
+  )
+
 function UnAuthenticatedApp(props) {
 
   return (
     <Router>
       <Switch>
-            <Route exact path="/" component={() => <LoginPage onNotify={props.onNotify}/>} />
-            <Route path="/reset-password" component={() => <ResetPassword onNotify={props.onNotify}/>}/>
-            <Route path="/register" component={() => <RegisterPage onNotify={props.onNotify}/>}/>
+            <PublicRoute exact path="/" component={() => <LoginPage onNotify={props.onNotify}/>} />
+            <PublicRoute path="/reset-password" component={() => <ResetPassword onNotify={props.onNotify}/>}/>
+            <PublicRoute path="/register" component={() => <RegisterPage onNotify={props.onNotify}/>}/>
       </Switch>
     </Router>
   );
